1ba4d63a14b4edab249a94d5d229da06d3d9e66a
Guilherme220294 <guilhermedosreissilva@yahoo.com.br>
2023-09-19 18:59:05 -0300

    exercicios feitos

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ Atribui o valor 10 à variável "i"
 Atribui a string "Mundo" à variável "j"
 */
 
+let i = 0;
+var j;
+
+var jj =
+console.log(i+10)
+console.log(j="Mundo")
 
 /*
 let x = 5; // Declara uma variável "x" e atribui o valor 5 a ela
@@ -24,6 +30,11 @@ const array = [1, 2, 3]; // Declara uma constante que armazena um array
 PI = 3.1415; // Gera um erro, pois não é possível reatribuir valor a uma constante
 
 
+
+
+
+//_______________________________________________________________________________
+
 2 - João comecou a trabalhar como desenvolvedor mas precisa de comprar um
 computador melhor. Achou um no preço de R$4.500,00 que é o valor que pode gastar
 no momento. Se ele comprar online vai ter um desconto de 20%.
@@ -36,23 +47,51 @@ Saída:
 Resultado do preço final no console.
 */
 
+// código da lição 2
+
+
+// possibilidade 1
+//let valorEnc = 4500;
+//let valorOn = 0.20;
+
+//console.log ("Valor do desconto na internet:",(valorEnc*valorOn))
+
+//console.log ("Valor final com desconto será de:",valorEnc-(valorEnc*valorOn))
+
+
+
+// Possibilidade 2
+
+//let valorEnc = 4500;
+//let valorOn = 0.20;
+
+//let desconto = valorEnc*valorOn
+//let valorFinal = valorEnc-(valorEnc*valorOn)
+
+//console.log("Valor do desconto na internet:",desconto)
+
+//console.log("Valor final com desconto será de:",valorFinal)
+
+
+//_____________________________________________________________________________________________
+
 
 //var armazenar informação dentro de uma variável (a var pode ser alterada enquanto o programa é executado)
 
-var olaMundo = "Oláa Mundooo!"
+//var olaMundo = "Oláa Mundooo!"
 
 
 
 //const mesmo conceito de variável (o conteúdo de uma const não pode ser alterado no decorrer do programa, ela é inalterável)
 
-const meuNomeEGui = 'meu nome é Gui'
+//const meuNomeEGui = 'meu nome é Gui'
 
  //let
 
- console.log(meuNomeEGui)
+ //console.log("meuNomeEGui")
 
- let idade = 30;
+ //let idade = 30;
 
- console.log(idade)
+ //console.log(idade)
 
  //testeeee
